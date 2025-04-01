eada9803561f74a762928a63ad92057c123a6915
tsald3 <tristansaldanha@gmail.com>
2025-03-31 18:02:48 -0700

    wip

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,131 +1,128 @@
--- Bootstrap lazy.nvim
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {
+    noremap = true,
+    silent = true
+})
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {
+    noremap = true,
+    silent = true
+})
+
+vim.opt.nu = true
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    local out = vim.fn.system({"git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath})
     if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out,                            "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
+        vim.api.nvim_echo({{"Failed to clone lazy.nvim:\n", "ErrorMsg"}, {out, "WarningMsg"},
+                           {"\nPress any key to exit..."}}, true, {})
         vim.fn.getchar()
         os.exit(1)
     end
 end
-vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
+vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Setup lazy.nvim
 require("lazy").setup({
-    spec = {
-        {
-            "tiagovla/tokyodark.nvim",
-            opts = {
-                -- custom options here
-            },
-            config = function(_, opts)
-                require("tokyodark").setup(opts) -- calling setup is optional
-                vim.cmd [[colorscheme tokyodark]]
-            end,
-        },
-        {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "neovim/nvim-lspconfig",
-        },
-        {
-            'nvim-telescope/telescope.nvim',
-            tag = '0.1.8',
-            dependencies = { 'nvim-lua/plenary.nvim' }
-        },
-        {
-            "nvim-treesitter/nvim-treesitter",
-            build = ":TSUpdate"
-        },
-        {
-            "nvim-tree/nvim-tree.lua",
-            dependencies = { "nvim-tree/nvim-web-devicons" },
-        },
+    spec = {{
+        "tiagovla/tokyodark.nvim",
+        opts = {},
+        config = function(_, opts)
+            require("tokyodark").setup(opts)
+            vim.cmd [[colorscheme tokyodark]]
+        end
+    }, {"williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim", "neovim/nvim-lspconfig"}, {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
+        dependencies = {'nvim-lua/plenary.nvim'}
+    }, {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
+    }, {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = {"nvim-tree/nvim-web-devicons"}
+    }},
+    install = {
+        colorscheme = {"tokyodark"}
     },
-    -- Configure any other settings here. See the documentation for more details.
-    -- colorscheme that will be used when installing plugins.
-    install = { colorscheme = { "tokyodark" } },
-    -- automatically check for plugin updates
-    checker = { enabled = true },
+    checker = {
+        enabled = true
+    }
 })
 
--- Configure LSP
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = {
-        "pyright", -- Python LSP
-        "clangd",
-        "awk_ls",
-        "bashls",
-        "dockerls",
-        "eslint",
-        "gopls",
-        "jsonls",
-        "marksman",
-        "pylsp",
-        "tflint",
-        "yamlls",
-    }
+    ensure_installed = {"pyright", "clangd", "awk_ls", "bashls", "dockerls", "eslint", "gopls", "jsonls", "marksman",
+                        "pylsp", "tflint", "yamlls"}
 })
 
 -- Configure LSP servers
 local lspconfig = require("lspconfig")
 
--- Configure NvimTree
 require("nvim-tree").setup({
-    sort_by = "case_sensitive",
+    sort = {
+        sorter = "case_sensitive"
+    },
     view = {
-        width = 30,
+        width = 30
     },
+    renderer = {
+        group_empty = true
+    },
+    filters = {
+        dotfiles = true
+    }
+}, {
+    update_focused_file = {
+        enable = true
+    }
 })
 
--- NvimTree keybindings
-local function on_attach(bufnr)
-    local api = require('nvim-tree.api')
-
-    -- Default mappings
-    api.config.mappings.default_on_attach(bufnr)
-end
-
-require("nvim-tree").setup({
-    on_attach = on_attach,
-    sort_by = "case_sensitive",
-    view = {
-        width = 30,
+require'nvim-web-devicons'.setup {
+    override = {
+        zsh = {
+            icon = "",
+            color = "#428850",
+            cterm_color = "65",
+            name = "Zsh"
+        }
     },
-})
-
--- Open NvimTree automatically on startup
-local function open_nvim_tree()
-    require("nvim-tree.api").tree.open()
-end
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
-
--- Python LSP configuration
-lspconfig.pyright.setup({
-    settings = {
-        python = {
-            analysis = {
-                autoSearchPaths = true,
-                useLibraryCodeForTypes = true,
-                diagnosticMode = "workspace",
-                inlayHints = true,
-            },
-            formatting = {
-                provider = "black",
-                indentSize = 4,
-            },
-        },
+    color_icons = true,
+    default = true,
+    strict = true,
+    override_by_filename = {
+        [".gitignore"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "Gitignore"
+        }
     },
-})
+    override_by_extension = {
+        ["log"] = {
+            icon = "",
+            color = "#81e043",
+            name = "Log"
+        }
+    },
+    override_by_operating_system = {
+        ["apple"] = {
+            icon = "",
+            color = "#A2AAAD",
+            cterm_color = "248",
+            name = "Apple"
+        }
+    }
+}
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.grep_string({
+        search = vim.fn.input("Grep > ")
+    });
+end)
