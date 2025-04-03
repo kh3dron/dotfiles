220f3bd48f2f7828fcfc34e900e847d9c0879bf8
Tristan Saldanha <tristansaldanha@gmail.com>
2025-04-02 23:11:08 -0700

    wip

--- wezterm.lua
+++ wezterm.lua
@@ -30,59 +30,6 @@ config.inactive_pane_hsb = {
     brightness = .8
 }
 
--- The filled in variant of the < symbol
-local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
-
--- The filled in variant of the > symbol
-local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
-
--- config.tab_bar_style = {
---     active_tab_left = wezterm.format {{
---         Background = {
---             Color = '#0b0022'
---         }
---     }, {
---         Foreground = {
---             Color = '#2b2042'
---         }
---     }, {
---         Text = SOLID_LEFT_ARROW
---     }},
---     active_tab_right = wezterm.format {{
---         Background = {
---             Color = '#0b0022'
---         }
---     }, {
---         Foreground = {
---             Color = '#2b2042'
---         }
---     }, {
---         Text = SOLID_RIGHT_ARROW
---     }},
---     inactive_tab_left = wezterm.format {{
---         Background = {
---             Color = '#0b0022'
---         }
---     }, {
---         Foreground = {
---             Color = '#1b1032'
---         }
---     }, {
---         Text = SOLID_LEFT_ARROW
---     }},
---     inactive_tab_right = wezterm.format {{
---         Background = {
---             Color = '#0b0022'
---         }
---     }, {
---         Foreground = {
---             Color = '#1b1032'
---         }
---     }, {
---         Text = SOLID_RIGHT_ARROW
---     }}
--- }
-
 config.keys = {{
     key = 'd',
     mods = 'CMD',
@@ -144,7 +91,22 @@ config.keys = {{
     action = act.SendKey { key = 'f', mods = 'ALT' },
   },
 
-
+  {
+    key = 'LeftArrow',
+    mods = 'OPT|SHIFT',
+    action = act.AdjustPaneSize { 'Left', 5 },
+  },
+  {
+    key = 'DownArrow',
+    mods = 'OPT|SHIFT',
+    action = act.AdjustPaneSize { 'Down', 5 },
+  },
+  { key = 'UpArrow', mods = 'OPT|SHIFT', action = act.AdjustPaneSize { 'Up', 5 } },
+  {
+    key = 'RightArrow',
+    mods = 'OPT|SHIFT',
+    action = act.AdjustPaneSize { 'Right', 5 },
+  },
   }
 
 return config
