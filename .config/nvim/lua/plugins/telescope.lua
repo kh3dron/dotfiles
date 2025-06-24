c93c7d7b63e66665ee1a3aad973566ad80f7e421
Tristan Saldanha <tristansaldanha@gmail.com>
2025-06-24 10:13:45 -0700

    wip

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,15 @@
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
-    dependencies = {'nvim-lua/plenary.nvim'}
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local telescope = require('telescope')
+        telescope.setup {
+            pickers = {
+                find_files = {
+                    hidden = true
+                }
+            }
+        }
+    end
 }
