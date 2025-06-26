34b112e62b8855779c631745dff36ee2cbabdedf
Tristan Saldanha <tristansaldanha@gmail.com>
2025-06-26 11:28:15 -0700

    tmux scroll

--- .config/nvim/lua/plugins/options.lua
+++ .config/nvim/lua/plugins/options.lua
@@ -3,5 +3,8 @@ vim.opt.nu = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- Enable system clipboard integration
+vim.opt.clipboard = "unnamedplus"
+
 vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
